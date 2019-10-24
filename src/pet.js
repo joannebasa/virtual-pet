7fb1002d2b9e085c169bf7511a6ae478d14827f4
Joanne G. Basa <joannegbasa@gmail.com>
2019-10-24 20:14:30 +0100

    added growUp method to pet prototype

--- src/pet.js
+++ src/pet.js
@@ -1,13 +1,25 @@
 
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+    // this.growUp = function() {
+    //   this.age +=1;
+    //}
 };
 
-const pet = {
-    name: 'Fido',
-};
+Pet.prototype.growUp = function() {
+  this.age +=1;
+}
+
+// const pet = {
+//     name: 'Fido',
+//     age: 0,
+// };
 
 /* or const pet = {};
 pet.name = 'Fido'; */
+
+const fido = new Pet('Fido');
+const rex = new Pet('Rex');
 
 module.exports = Pet;
